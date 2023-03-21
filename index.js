55521d4ca7e09a79ad9887dd8a82ef5b2a3220b5
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-03-21 21:19:37 +0000

    Push functional shit

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import { io } from "socket.io-client"
+
+export class Command {
+    constructor(name, exec){
+        this.name = name
+        this._exec = exec
+    }
+    exec(...params){
+        this._exec(...params)
+    }
+}
 export default class {
     constructor(){
     }
-    connect(url = "wss://windows96.net:4096"){
+    connect(nick, url = process.env.URL || "wss://windows96.net:4096"){
         this.SOCKET = new io(url)
         return new Promise((resolve, reject) => {
-            this.SOCKET.on("open", () => {
+            //this.SOCKET.on("open", () => {
+                this.SOCKET.emit("auth", {user: nick})
                 this.SOCKET.on("auth-complete", userId => {
                     this.useId = userId
                     resolve(this)
                 })
-            })
+                this.SOCKET.on("auth-error", e => {
+                    console.error(e.reason)
+                    reject(e)
+                })
+            //})
         })
     }
     send(msg = ""){
@@ -30,5 +45,8 @@ export default class {
                 resolve()
             })
         })
+    }
+    registerCommand(name, exec){
+        return new Command(name, exec)
     }
 }
