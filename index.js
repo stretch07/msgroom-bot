fe263c806134161e4a426e55919001ccbafb0b0c
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-03-21 21:22:57 +0000

    JSDoc connect

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 import { io } from "socket.io-client"
 
 export class Command {
-    constructor(name, exec){
+    constructor(name, exec) {
         this.name = name
         this._exec = exec
     }
-    exec(...params){
+    exec(...params) {
         this._exec(...params)
     }
 }
 export default class {
-    constructor(){
+    constructor() {
     }
-    connect(nick, url = process.env.URL || "wss://windows96.net:4096"){
-        this.SOCKET = new io(url)
+    /**
+     * 
+     * @param {string} nick Nickname of the bot to initially connect
+     * @param {URL} url URL of the bot to connect to. Leave blank for default Windows 96 msgroom
+     */
+    connect(nick, url = new URL("wss://windows96.net:4096")) {
+        this.SOCKET = new io(url.href)
         return new Promise((resolve, reject) => {
-            //this.SOCKET.on("open", () => {
-                this.SOCKET.emit("auth", {user: nick})
-                this.SOCKET.on("auth-complete", userId => {
-                    this.useId = userId
-                    resolve(this)
-                })
-                this.SOCKET.on("auth-error", e => {
-                    console.error(e.reason)
-                    reject(e)
-                })
-            //})
+            this.SOCKET.emit("auth", { user: nick })
+            this.SOCKET.on("auth-complete", userId => {
+                this.useId = userId
+                resolve(this)
+            })
+            this.SOCKET.on("auth-error", e => {
+                reject(e)
+            })
         })
     }
-    send(msg = ""){
+    send(msg = "") {
         this.SOCKET.emit("message", {
             type: "text",
             value: msg
         })
         return this
     }
-    changeNick(nick = "nick"){
+    changeNick(nick = "nick") {
         this.SOCKET.emit("change-user", nick)
         return this
     }
-    waitUntil(event){
+    waitUntil(event) {
         return new Promise((resolve) => {
             this.SOCKET.on(event, () => {
                 resolve()
             })
         })
     }
-    registerCommand(name, exec){
+    registerCommand(name, exec) {
         return new Command(name, exec)
     }
 }
