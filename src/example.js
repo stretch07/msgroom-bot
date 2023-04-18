7e89a4c93b793e294ec75b42efb6f37f54094560
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-04-18 22:04:23 +0000

    Fix example.js, update readme

--- src/example.js
+++ src/example.js
@@ -12,8 +12,11 @@ bot.send("Hello! This is msgroom-bot made by its-pablo. If you are seeing this, 
 console.log("Default message sent")
 
 const cmse = bot.registerCommandSet("ok!")
-cmse.registerCommand("say", async (...said) => {
+cmse.registerCommand("say", (...args) => {
     //users can say multiple things separated by spaces, so lets take all the arguments
     //and join them with spaces.
-    bot.send("You said: " + said.join(" "))
+    bot.send("You said: " + args.join(" "))
+})
+cmse.registerCommand("nick", (...args) => {
+    bot.changeNick(args[0])
 })
