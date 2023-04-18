2d6f1b7b3e02d724f03d4f42d458dacaa1464297
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-04-18 21:46:25 +0000

    Fix example.js

--- src/example.js
+++ src/example.js
@@ -2,19 +2,18 @@ import MsgroomBot from "./index.js";
 const bot = new MsgroomBot
 
 try {
-    await bot.connect("msgroom-bot")
+    bot.connect("msgroom-bot")
 } catch(e) {
-    process.exit(1, e)
+    process.exit(1)
 }
 console.log("Connected")
 
-await bot.send("Hello! This is msgroom-bot made by its-pablo. If you are seeing this, everything is working properly. Try not to run this file many times because staff doesn't like spam.")
+bot.send("Hello! This is msgroom-bot made by its-pablo. If you are seeing this, everything is working properly. Try not to run this file many times because staff doesn't like spam.")
 console.log("Default message sent")
 
 const cmse = bot.registerCommandSet("ok!")
-cmse.registerCommand("say", async (said) => {
-    await bot.send("you said " + said)
+cmse.registerCommand("say", async (...said) => {
+    //users can say multiple things separated by spaces, so lets take all the arguments
+    //and join them with spaces.
+    bot.send("You said: " + said.join(" "))
 })
-
-//test that
-await bot.send("ok!say hi")
